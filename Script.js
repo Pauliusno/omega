8fa08a9d93ee42a632ad5ead09effd6a7d66b32f
Pauliusno <paulius.pocius@icloud.com>
2023-08-29 20:32:58 +0300

    added page

---
+++ Script.js
@@ -0,0 +1,45 @@
+const serviceWrapper = document.getElementById('services');
+
+const buildServiceWrapper = (omega) => {
+    const wrapper = document.createElement("a");
+    wrapper.setAttribute('class', 'services-wrapper');
+
+    const imageDiv = document.createElement('div'); // Create a div for the image
+    // imageDiv.setAttribute('class', 'omega-image-div'); // You can style this div if needed
+
+    const image = document.createElement('img');
+    image.setAttribute('class', 'omega-image');
+    image.src = omega.img_url;
+    imageDiv.appendChild(image);
+
+    const contentDiv = document.createElement('div'); // Create a div for the content
+    // contentDiv.setAttribute('class', 'omega-content-div'); // You can style this div if needed
+
+    const title = document.createElement("h1");
+    title.innerHTML = omega.title;
+    const location = document.createElement('p');
+    location.innerHTML = omega.location;
+    const price = document.createElement('p');
+    price.innerHTML = 'Kaina ' + omega.price;
+
+    contentDiv.append(title);
+    contentDiv.append(location);
+    contentDiv.append(price);
+
+    wrapper.append(imageDiv);
+    wrapper.append(contentDiv);
+
+    return wrapper;
+}
+
+const getAllServices = async () => {
+    const response = await fetch('https://64edd0bb1f8721827141cb01.mockapi.io/omega');
+    const services = await response.json();
+
+    services.forEach(omega => {
+        const wrapper = buildServiceWrapper(omega);
+        serviceWrapper.appendChild(wrapper);
+    });
+}
+
+getAllServices();
