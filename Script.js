f234eb4821450e771fa8eac65ab653d710112dd4
Pauliusno <paulius.pocius@icloud.com>
2023-08-30 15:19:03 +0300

    updated about js

--- Script.js
+++ Script.js
@@ -4,18 +4,24 @@ const buildServiceWrapper = (omega) => {
     const wrapper = document.createElement("a");
     wrapper.setAttribute('class', 'services-wrapper');
 
+
     const imageDiv = document.createElement('div'); // Create a div for the image
     // imageDiv.setAttribute('class', 'omega-image-div'); // You can style this div if needed
 
     const image = document.createElement('img');
     image.setAttribute('class', 'omega-image');
     image.src = omega.img_url;
-    imageDiv.appendChild(image);
 
     const contentDiv = document.createElement('div'); // Create a div for the content
     // contentDiv.setAttribute('class', 'omega-content-div'); // You can style this div if needed
 
-    const title = document.createElement("h1");
+    wrapper.href = './about.html'
+    wrapper.addEventListener('click', ()=>{
+        localStorage.setItem('omegaId',omega.id)
+    })
+
+
+    const title = document.createElement("h2");
     title.innerHTML = omega.title;
     const location = document.createElement('p');
     location.innerHTML = omega.location;
@@ -25,6 +31,7 @@ const buildServiceWrapper = (omega) => {
     contentDiv.append(title);
     contentDiv.append(location);
     contentDiv.append(price);
+    imageDiv.append(image);
 
     wrapper.append(imageDiv);
     wrapper.append(contentDiv);
@@ -35,10 +42,11 @@ const buildServiceWrapper = (omega) => {
 const getAllServices = async () => {
     const response = await fetch('https://64edd0bb1f8721827141cb01.mockapi.io/omega');
     const services = await response.json();
-
+    
+    services.sort((a, b) => a.price - b.price);
     services.forEach(omega => {
         const wrapper = buildServiceWrapper(omega);
-        serviceWrapper.appendChild(wrapper);
+        serviceWrapper.append(wrapper);
     });
 }
 
---
+++ about.js
@@ -0,0 +1,63 @@
+
+
+const omegaId = localStorage.getItem('omegaId');
+console.log(omegaId);
+
+const addServiceToScreen = (omega) => {
+    const title = document.getElementById('about-title');
+    title.innerHTML = omega.title;
+
+    const location = document.getElementById('about-location');
+    location.innerHTML = omega.location;
+
+    const description = document.getElementById('about-description');
+    description.innerHTML = omega.description;
+    const price = document.getElementById('about-price');
+    price.innerHTML = 'Kaina ' + omega.price; // Changed "ration" to "price"
+    const image = document.getElementById('about-image');
+    image.src = omega.img_url;
+}
+
+
+    const getOneService = async () => {
+        if (omegaId) {
+            try {
+                const response = await fetch('https://64edd0bb1f8721827141cb01.mockapi.io/omega/' + omegaId);
+                if (response.ok) {
+                    const omega = await response.json();
+                    console.log('Omega data from API:', omega); // Debug line
+                    addServiceToScreen(omega);
+                } else {
+                    console.error('Error fetching data:', response.status);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        } else {
+            console.error('omegaId is not defined');
+        }
+    }
+    
+
+
+getOneService();
+
+const deleteButton = document.getElementById('delete-button')
+deleteButton.addEventListener('click',async()=>{
+    try{
+    const response = await fetch('https://64edd0bb1f8721827141cb01.mockapi.io/omega/' + omegaId,{
+        method: 'DELETE',        
+    })
+    const data = await response.json()
+    if(data){
+        const infomsg = document.getElementById('info')
+        infomsg.innerHTML = 'Service was deleted!!!!'
+        setTimeout(()=>{
+            window.location.replace("./index.html")
+        },3000)
+    }
+} catch(err){
+    const infomsg = document.getElementById('infomsg')
+        infomsg.innerHTML = 'Service was not deleted!!!!'
+}
+})
